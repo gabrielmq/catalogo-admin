ebd1e5ed5e9cfdce2bf891e045497a6f3c255a3f
Gabriel Marques de Souza <gabrielmqs04@gmail.com>
2022-06-06 13:28:42 -0300

    adiciona configs iniciais do spring boot

---
+++ infrastructure/src/main/java/com/fullcycle/catalogo/admin/infrastructure/configuration/WebServerConfiguration.java
@@ -0,0 +1,9 @@
+package com.fullcycle.catalogo.admin.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.fullcycle.catalogo.admin")
+public class WebServerConfiguration {
+}
