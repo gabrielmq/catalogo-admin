4f5eb9e175a8c193c172c82f46cf0f463f0234c9
Gabriel Marques de Souza <gabrielmqs04@gmail.com>
2022-06-09 13:14:59 -0300

    adiciona testes de marshall/unmarshall request/response

---
+++ infrastructure/src/test/java/com/fullcycle/catalogo/admin/JacksonTest.java
@@ -0,0 +1,25 @@
+package com.fullcycle.catalogo.admin;
+
+import com.fullcycle.catalogo.admin.infrastructure.configuration.ObjectMapperConfiguration;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static org.springframework.context.annotation.FilterType.ASSIGNABLE_TYPE;
+
+@Inherited
+@Target(TYPE)
+@Retention(RUNTIME)
+@ActiveProfiles("test")
+@JsonTest(includeFilters = {
+    @ComponentScan.Filter(type = ASSIGNABLE_TYPE, classes = ObjectMapperConfiguration.class)
+})
+public @interface JacksonTest {
+}
