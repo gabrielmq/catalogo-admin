e677157edfd867273e692f65178d14f7e2b44092
Gabriel Marques de Souza <gabrielmqs04@gmail.com>
2022-06-13 19:28:40 -0300

    adiciona restante dos testes e2e

--- infrastructure/src/test/java/com/fullcycle/catalogo/admin/E2ETest.java
+++ infrastructure/src/test/java/com/fullcycle/catalogo/admin/E2ETest.java
@@ -2,15 +2,20 @@ package com.fullcycle.catalogo.admin;
 
 import com.fullcycle.catalogo.admin.infrastructure.configuration.WebServerConfiguration;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.lang.annotation.*;
 
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 @Inherited
+@Target(TYPE)
+@Retention(RUNTIME)
+@AutoConfigureMockMvc
 @ActiveProfiles("test-e2e")
-@Target(ElementType.TYPE)
-@Retention(RetentionPolicy.RUNTIME)
 @ExtendWith(MySQLCleanUpExtension.class)
 @SpringBootTest(classes = WebServerConfiguration.class)
 public @interface E2ETest {
