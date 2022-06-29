149d61252222e45cc37fbc0fd46cf3d51c71f092
Gabriel Marques de Souza <gabrielmqs04@gmail.com>
2022-06-29 13:29:48 -0300

    adiciona testes integrados usecase list genre

---
+++ infrastructure/src/test/java/com/fullcycle/catalogo/admin/application/genre/retrieve/list/ListGenreUseCaseIT.java
@@ -0,0 +1,87 @@
+package com.fullcycle.catalogo.admin.application.genre.retrieve.list;
+
+import com.fullcycle.catalogo.admin.IntegrationTest;
+import com.fullcycle.catalogo.admin.domain.genre.Genre;
+import com.fullcycle.catalogo.admin.domain.genre.GenreGateway;
+import com.fullcycle.catalogo.admin.domain.pagination.SearchQuery;
+import com.fullcycle.catalogo.admin.infrastructure.genre.persistence.GenreJpaEntity;
+import com.fullcycle.catalogo.admin.infrastructure.genre.persistence.GenreRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
+@IntegrationTest
+public class ListGenreUseCaseIT {
+
+    @Autowired
+    private ListGenreUseCase useCase;
+
+    @Autowired
+    private GenreGateway genreGateway;
+
+    @Autowired
+    private GenreRepository genreRepository;
+
+    @Test
+    public void givenAValidQuery_whenCallsListGenre_thenShouldReturnGenres() {
+        final var genres = List.of(
+            Genre.newGenreWith("Ação", true),
+            Genre.newGenreWith("Aventura", true)
+        );
+
+        genreRepository.saveAllAndFlush(
+            genres.stream().map(GenreJpaEntity::from).toList()
+        );
+
+        final var expectedPage = 0;
+        final var expectedPerPage = 10;
+        final var expectedTerms = "A";
+        final var expectedSort = "createdAt";
+        final var expectedDirection = "asc";
+        final var expectedTotal = 2;
+
+        final var expectedItems = genres.stream()
+                .map(GenreListOutput::from)
+                .toList();
+
+        final var aQuery =
+            new SearchQuery(expectedPage, expectedPerPage, expectedTerms, expectedSort, expectedDirection);
+
+        final var actualOutput = useCase.execute(aQuery);
+
+        assertEquals(expectedPage, actualOutput.currentPage());
+        assertEquals(expectedPerPage, actualOutput.perPage());
+        assertEquals(expectedTotal, actualOutput.total());
+        assertTrue(
+            expectedItems.size() == actualOutput.items().size()
+            && expectedItems.containsAll(actualOutput.items())
+        );
+    }
+
+    @Test
+    public void givenAValidQuery_whenCallsListGenreAndResultIsEmpty_thenShouldReturnGenres() {
+        final var expectedPage = 0;
+        final var expectedPerPage = 10;
+        final var expectedTerms = "A";
+        final var expectedSort = "createdAt";
+        final var expectedDirection = "asc";
+        final var expectedTotal = 0;
+
+        final var expectedItems = List.<GenreListOutput>of();
+
+        final var aQuery =
+            new SearchQuery(expectedPage, expectedPerPage, expectedTerms, expectedSort, expectedDirection);
+
+        final var actualOutput = useCase.execute(aQuery);
+
+        assertEquals(expectedPage, actualOutput.currentPage());
+        assertEquals(expectedPerPage, actualOutput.perPage());
+        assertEquals(expectedTotal, actualOutput.total());
+        assertEquals(expectedItems, actualOutput.items());
+    }
+}
