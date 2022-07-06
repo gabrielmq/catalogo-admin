e9f3e1faaf50b3b80df0a6ee14bea838f1af89f3
Gabriel Marques de Souza <gabrielmqs04@gmail.com>
2022-07-06 18:20:09 -0300

    adiciona interface de GenreAPI

---
+++ infrastructure/src/main/java/com/fullcycle/catalogo/admin/infrastructure/genre/presenters/GenreApiPresenter.java
@@ -0,0 +1,30 @@
+package com.fullcycle.catalogo.admin.infrastructure.genre.presenters;
+
+import com.fullcycle.catalogo.admin.application.genre.retrieve.get.GenreOutput;
+import com.fullcycle.catalogo.admin.application.genre.retrieve.list.GenreListOutput;
+import com.fullcycle.catalogo.admin.infrastructure.genre.models.GenreListResponse;
+import com.fullcycle.catalogo.admin.infrastructure.genre.models.GenreResponse;
+
+public interface GenreApiPresenter {
+    static GenreResponse present(final GenreOutput output) {
+        return new GenreResponse(
+            output.id(),
+            output.name(),
+            output.categories(),
+            output.isActive(),
+            output.createdAt(),
+            output.updatedAt(),
+            output.deletedAt()
+        );
+    }
+
+    static GenreListResponse present(final GenreListOutput output) {
+        return new GenreListResponse(
+                output.id(),
+                output.name(),
+                output.isActive(),
+                output.createdAt(),
+                output.deletedAt()
+        );
+    }
+}
