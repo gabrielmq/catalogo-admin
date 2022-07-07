7eb6dc5b77494b78a9148449f315b9dde1e157cc
Gabriel Marques de Souza <gabrielmqs04@gmail.com>
2022-07-07 18:59:12 -0300

    implementa api de genres

--- infrastructure/src/main/java/com/fullcycle/catalogo/admin/infrastructure/genre/models/CreateGenreRequest.java
+++ infrastructure/src/main/java/com/fullcycle/catalogo/admin/infrastructure/genre/models/CreateGenreRequest.java
@@ -2,11 +2,20 @@ package com.fullcycle.catalogo.admin.infrastructure.genre.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public record CreateGenreRequest(
     @JsonProperty("name") String name,
     @JsonProperty("categories_id") List<String> categories,
     @JsonProperty("is_active") Boolean active
 ) {
+    public boolean isActive() {
+        return Objects.requireNonNullElse(active, true);
+    }
+
+    public List<String> categories() {
+        return Objects.requireNonNullElse(categories, Collections.emptyList());
+    }
 }
