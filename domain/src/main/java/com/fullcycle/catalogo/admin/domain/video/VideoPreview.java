f678aa4259b1d08b5d24758af60f39847d6bd95a
Gabriel Marques de Souza <gabrielmqs04@gmail.com>
2022-11-03 20:07:20 -0300

    refatora VideoPreview

--- domain/src/main/java/com/fullcycle/catalogo/admin/domain/video/VideoPreview.java
+++ domain/src/main/java/com/fullcycle/catalogo/admin/domain/video/VideoPreview.java
@@ -4,9 +4,18 @@ import java.time.Instant;
 
 public record VideoPreview(
     String id,
-    String description,
     String title,
+    String description,
     Instant createdAt,
     Instant updatedAt
 ) {
+    public static VideoPreview from(final Video aVideo) {
+        return new VideoPreview(
+            aVideo.getId().getValue(),
+            aVideo.getTitle(),
+            aVideo.getDescription(),
+            aVideo.getCreatedAt(),
+            aVideo.getUpdatedAt()
+        );
+    }
 }
