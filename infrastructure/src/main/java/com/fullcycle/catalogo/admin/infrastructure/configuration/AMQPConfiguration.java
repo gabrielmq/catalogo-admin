7adb4eed9abf9951ad1c3da3dccaa9c35c739985
Gabriel Marques de Souza <gabrielmqs04@gmail.com>
2022-11-23 13:14:53 -0300

    adiciona config rabbitmq admin

--- infrastructure/src/main/java/com/fullcycle/catalogo/admin/infrastructure/configuration/AMQPConfiguration.java
+++ infrastructure/src/main/java/com/fullcycle/catalogo/admin/infrastructure/configuration/AMQPConfiguration.java
@@ -2,7 +2,9 @@ package com.fullcycle.catalogo.admin.infrastructure.configuration;
 
 import com.fullcycle.catalogo.admin.infrastructure.configuration.annontations.VideoCreatedQueue;
 import com.fullcycle.catalogo.admin.infrastructure.configuration.annontations.VideoEncodedQueue;
+import com.fullcycle.catalogo.admin.infrastructure.configuration.annontations.VideoEvents;
 import com.fullcycle.catalogo.admin.infrastructure.configuration.properties.amqp.QueueProperties;
+import org.springframework.amqp.core.*;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,5 +24,46 @@ public class AMQPConfiguration {
     @ConfigurationProperties("amqp.queues.video-encoded")
     public QueueProperties videoEncodedQueueProperties() {
         return new QueueProperties();
+    }
+
+    @Configuration
+    static class Admin {
+        @Bean
+        @VideoEvents
+        public Exchange videoEventsExchange(@VideoCreatedQueue final QueueProperties props) {
+            return new DirectExchange(props.getExchange());
+        }
+
+        @Bean
+        @VideoCreatedQueue
+        public Queue videoCreatedQueue(@VideoCreatedQueue final QueueProperties props) {
+            return new Queue(props.getQueue());
+        }
+
+        @Bean
+        @VideoCreatedQueue
+        public Binding videoCreatedBinding(
+            @VideoEvents final DirectExchange exchange,
+            @VideoCreatedQueue final Queue queue,
+            @VideoCreatedQueue final QueueProperties props
+        ) {
+            return BindingBuilder.bind(queue).to(exchange).with(props.getRoutingKey());
+        }
+
+        @Bean
+        @VideoEncodedQueue
+        public Queue videoEncodedQueue(@VideoEncodedQueue final QueueProperties props) {
+            return new Queue(props.getQueue());
+        }
+
+        @Bean
+        @VideoEncodedQueue
+        public Binding videoEncodedBinding(
+            @VideoEvents final DirectExchange exchange,
+            @VideoEncodedQueue final Queue queue,
+            @VideoEncodedQueue final QueueProperties props
+        ) {
+            return BindingBuilder.bind(queue).to(exchange).with(props.getRoutingKey());
+        }
     }
 }
